3d6e0f0187caede399d7c3527cd997be19695537
Sunshine <steixeira1337@gmail.com>
2022-07-01 09:36:03 -0300

    feat: "some changes"

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 3000;
 
-const tours = require('./tours.json')
+const tours = require('./src/tours/tours.json')
 
 
 server.get('/tours', (req, res) => {
